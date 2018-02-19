714b9ef8854d0aec57308292ee16c0a4448acc00
Yoav <yoav.test.git@gmail.com>
2018-02-19 14:12:06 +0200

    audited the comments

--- DirectoryToList.py
+++ DirectoryToList.py
@@ -3,14 +3,14 @@ import os
 
 def create_list_from_directory_path(directory_path):
     """
-    This function creates a list of all subdirectories of a certain directory received.
+    This function creates a list of all sub directories of a certain directory received.
     The function first checks if the path given as input is a valid path and if it
     refers to a directory, If not it throws a corresponding exception.
     If the tests mentioned above are correct then the function creates a list from
-    all subdirectories using the os.walk method with list comprehension.
-    The function returns a list of all subdirectories(full paths).
+    all sub directories using the os.walk method with list comprehension.
+    The function returns a list of all sub directories(full paths).
     :param directory_path: The path of the directory.
-    :return: List of all subdirectories of a directory path given as input.
+    :return: List of all sub directories of a directory path given as input.
     """
     # checks if the path exists.
     if os.path.exists(directory_path) is False:
--- DirectoryToPathImplementingOSWalk.py
+++ DirectoryToPathImplementingOSWalk.py
@@ -4,25 +4,25 @@ import os
 OS_FILE_SEP = os.sep
 
 
-def generate_subdirectories_list(path, directories_list):
+def generate_sub_directories_list(path, directories_list):
     """
-    This function updates a list received as input with all subdirectories
+    This function updates a list received as input with all sub directories
     full paths.
     The function runs on the directory tree in a dfs manner.
     It checks if the path received is a directory path, If so it runs in a for
-    loop on all the subdirectories in the current directory(It gets the list using
+    loop on all the sub directories in the current directory(It gets the list using
     the os.listdir method) and it calls The function recursively with the path, file separator
-    and the subdirectory name all concatenated.
-    when the function gets to a "dead end" meaning no subdirectory exists, It appends it to
-    the subdirectories list.
-    :param path: The current path on which we want to append its subdirectories.
-    :param directories_list: The list of the subdirectories path.
+    and the sub directory name all concatenated.
+    when the function gets to a "dead end" meaning no sub directory exists, It appends it to
+    the sub directories list.
+    :param path: The current path on which we want to append its sub directories.
+    :param directories_list: The list of the sub directories path.
     :return: The function doesn't return anything it just updates the list.
     """
     if os.path.isdir(path):
         # os.listdir() returns a list with all sub directories in current level.
         for dir in os.listdir(path):
-            generate_subdirectories_list(path + OS_FILE_SEP + dir, directories_list)
+            generate_sub_directories_list(path + OS_FILE_SEP + dir, directories_list)
         directories_list.append(path)
 
 
@@ -34,7 +34,7 @@ def directory_path_to_list(directory_path):
     calls another function that updates the entire list of paths and if the list contains
     the original path then it removes it.
     :param directory_path: The path of the directory.
-    :return: The function returns a list of all paths of the subdirectories of the path given
+    :return: The function returns a list of all paths of the sub directories of the path given
     as input.
     """
     # checks if the path exists.
@@ -49,7 +49,7 @@ def directory_path_to_list(directory_path):
     directories_list = []
     directory_path = os.path.abspath(directory_path)
     # calls the function that updates the list.
-    generate_subdirectories_list(path=directory_path, directories_list=directories_list)
+    generate_sub_directories_list(path=directory_path, directories_list=directories_list)
 
     # removes the original directory if it is in the list.
     if directory_path in directories_list:
